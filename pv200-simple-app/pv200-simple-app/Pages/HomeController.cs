7538e30066f54a37b27cc4d47295cb6dee15f916
Roman <romdrak@email.cz>
2022-03-21 09:03:04 +0100

    feat: initial app

---
+++ pv200-simple-app/pv200-simple-app/Pages/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace pv200_simple_app.Pages
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            string name = "Makrel";
+            if (!String.IsNullOrEmpty(HttpContext.Request.Query["name"]))
+                name = HttpContext.Request.Query["name"];
+
+            return Content("Name from query string: " + name);
+        }
+    }
+}
